77feee108d17cfb3465cc64b4678fffcda32f705
Matt Massicotte <85322+mattmassicotte@users.noreply.github.com>
2019-09-11 12:37:50 -0400

    Added some documentation, bumped version

--- Rearrange/NSRange+Shift.swift
+++ Rearrange/NSRange+Shift.swift
@@ -9,31 +9,58 @@
 import Foundation
 
 extension NSRange {
+    /// Returns an NSRange offset by a delta
+    ///
+    /// This function returns a new NSRange, with the location offset
+    /// by delta. Conceptually, this is like sliding the receiver.
+    ///
+    /// If the location + delta would result in a negative number,
+    /// this returns nil.
     public func shifted(by delta: Int) -> NSRange? {
-        if (delta < 0 && labs(delta) > location) {
+        let newLocation = location + delta
+
+        if newLocation < 0 {
             return nil
         }
 
-        return NSMakeRange(location + delta, length)
+        return NSRange(location: newLocation, length: length)
     }
 
+    /// Returns an NSRange shifted by location
+    ///
+    /// This function shifts the starting location by delta,
+    /// and adjusts the corresponding length. This operation
+    /// affects only the starting location. The result will
+    /// have the same ending position.
+    ///
+    /// If the resulting length or location would be negative,
+    /// or if the delta is larger than the length this returns nil.
     public func shifted(startBy delta: Int) -> NSRange? {
-        if delta > length {
+        let newLocation = location + delta
+        let newLength = length - delta
+
+        if newLocation < 0 || newLength < 0 {
             return nil
         }
 
-        if (delta < 0 && labs(delta) > location) {
-            return nil
-        }
-
-        return NSMakeRange(location + delta, length - delta)
+        return NSRange(location: newLocation, length: newLength)
     }
 
+    /// Returns an NSRange shifted by length
+    ///
+    /// This function shifts the length by delta. This operation
+    /// affects only the ending position. The result will
+    /// have the same location.
+    ///
+    /// If the resulting length would be negative,
+    /// this returns nil.
     public func shifted(endBy delta: Int) -> NSRange? {
-        if (delta < 0 && labs(delta) > length) {
+        let newLength = length + delta
+
+        if newLength < 0 {
             return nil
         }
 
-        return NSMakeRange(location, length + delta)
+        return NSRange(location: location, length: newLength)
     }
 }
