1e6952244d5cf5b5bf4bb5f69a4d9810e6007658
Matt <85322+mattmassicotte@users.noreply.github.com>
2023-12-08 09:24:00 -0500

    adding <=, >, >= for NSTextLocation

--- Sources/Rearrange/NSTextLocation+Comparable.swift
+++ Sources/Rearrange/NSTextLocation+Comparable.swift
@@ -15,6 +15,18 @@ extension NSTextLocation {
 	public static func == (lhs: Self, rhs: Self) -> Bool {
 		return lhs.compare(rhs) == .orderedSame
 	}
+
+	public static func <= (lhs: Self, rhs: Self) -> Bool {
+		return lhs < rhs || lhs == rhs
+	}
+
+	public static func > (lhs: Self, rhs: Self) -> Bool {
+		return lhs.compare(rhs) == .orderedDescending
+	}
+
+	public static func >= (lhs: Self, rhs: Self) -> Bool {
+		return lhs > rhs || lhs > rhs
+	}
 }
 
 #endif
