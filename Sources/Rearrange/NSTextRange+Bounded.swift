2bfff9a3806321aabf2e57b68f561b70df639306
Matt <85322+mattmassicotte@users.noreply.github.com>
2025-05-08 11:01:03 -0400

    Guard NSTextRange, NSTextLocation

--- Sources/Rearrange/NSTextRange+Bounded.swift
+++ Sources/Rearrange/NSTextRange+Bounded.swift
@@ -4,8 +4,10 @@ import AppKit
 import UIKit
 #endif
 
+#if os(macOS) || os(iOS) || os(tvOS) || os(visionOS)
 @available(iOS 15.0, macOS 12.0, tvOS 15.0, macCatalyst 15.0, *)
 extension NSTextRange : Bounded {
 	public var lowerBound: NSTextLocation { location }
 	public var upperBound: NSTextLocation { endLocation }
 }
+#endif
